d2cfc04aa619dd9054e19c5371e98688e96217ba
shshwtsuthar <shshwtsuthar@gmail.com>
2025-03-04 09:06:41 +1100

    Core Classes

--- objects.tsx
+++ objects.tsx
@@ -1,21 +1,41 @@
-class Velocity{
-    x: number;
-    y: number;
+import { GPU } from './gpu.js';
 
-    constructor(x: number, y: number){
-        
+const gpu = new GPU();
+
+// Creating the 2-dimensional velocity vector class
+class Velocity{
+    vx: number;
+    vy: number;
+
+    constructor(vx: number, vy: number){
+        this.vx = vx;
+        this.vy = vy;
     }
 }
 
+// Creating the 2-dimensional velocity vector class
+class Acceleration{
+    ax: number;
+    ay: number;
+
+    constructor(ax: number, ay: number){
+        this.ax = ax;
+        this.ay = ay;
+    }
+}
+
+// Creating the entity class 
 class Entity{
     x: number;
     y: number;
-    acceleration: number[];
-    velocity: number[];
+    acceleration: Acceleration;
+    velocity: Velocity;
 
 
-    constructor(x: number, y: number){
+    constructor(x: number, y: number, vx: number, vy: number, ax: number, ay: number){
         this.x = x;
         this.y = y;
+        this.velocity = new Velocity(vx, vy);
+        this.acceleration = new Acceleration(ax, ay);
     }
 }
